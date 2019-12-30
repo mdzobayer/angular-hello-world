00fc55fa7f90dc8e3392f45d5ec06b77f0f18315
mdzobayer <mdzobayer@rocketmail.com>
2019-12-30 15:44:18 +0600

    Add new compoment with input output

---
+++ src/app/favorite/favorite.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+
+@Component({
+  selector: 'app-favorite',
+  templateUrl: './favorite.component.html',
+  styleUrls: ['./favorite.component.css']
+})
+
+export class FavoriteComponent implements OnInit {
+  @Input() title: string;
+  @Input() obj: any;
+
+  @Output() change = new EventEmitter();
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onClick() {
+    this.change.emit({message: "A message from Child"});
+  }
+  ParentToChild() {
+    console.log(this.obj);
+  }
+
+}

93a05f6e4127e8779fb5e5164307460564078622
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-19 10:22:57 +0600

    Add form validation input upon submit

--- src/app/signup-form/signup-form.component.ts
+++ src/app/signup-form/signup-form.component.ts
@@ -20,4 +20,16 @@ export class SignupFormComponent {
   get username() {
     return this.form.get('username');
   }
+
+  login() {
+    // let isValid = authService.login(this.form.value);
+    // if (!isValid) {
+    //   this.form.setErrors({
+    //     invalidLogin: true
+    //   });
+    // }
+    this.form.setErrors({
+      invalidLogin: true
+    });
+  }
 }

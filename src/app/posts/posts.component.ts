6f2cc48196efccfe411ff88eb1d6a80a7173b5f5
mdzobayer <muhammad.zobayer@selise.ch>
2020-01-20 10:52:11 +0600

    Create data using http post method

--- src/app/posts/posts.component.ts
+++ src/app/posts/posts.component.ts
@@ -8,14 +8,27 @@ import { Http } from '@angular/http';
 })
 export class PostsComponent implements OnInit {
   posts:any[];
-  constructor(http: Http) {
-    http.get('http://jsonplaceholder.typicode.com/posts').subscribe(response => {
+  private url = 'http://jsonplaceholder.typicode.com/posts';
+
+  constructor(private http: Http) {
+    http.get(this.url).subscribe(response => {
       //console.log(response.json());
       this.posts = response.json();
     });
   }
 
   ngOnInit() {
+  }
+
+  createPost(input: HTMLInputElement) {
+    let post = {title: input.value}
+    input.value = '';
+    this.http.post(this.url, JSON.stringify(post)).subscribe(response =>{
+      post['id'] = response.json().id;
+//      this.posts.push(post);    // Add end of the list;
+      this.posts.splice(0, 0, post);  // Add begin of the list
+      console.log(response.json());
+    });
   }
 
 }
